939ca3ffd6da3e3d155a19c0d7391cb8d77a1b8c
SpoofingRB <rtempest820@gmail.com>
2026-09-05 19:40:06 -0300

    Alterando Quem-Somos.html

--- obj/Debug/net8.0/appointmentapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/appointmentapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appointmentapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e29fe654e1b368d76054e4d3108650044d9e90b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d7320024ab8ea9a665cc42e5c7ccfa59ce49d41")]
 [assembly: System.Reflection.AssemblyProductAttribute("appointmentapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appointmentapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
